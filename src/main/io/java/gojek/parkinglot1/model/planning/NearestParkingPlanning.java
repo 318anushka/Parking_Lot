8ccb00679d975805b67dbe9fa14742993ce2106d
Anushka Singh <anushka318singh@gmail.com>
2019-12-28 18:25:09 +0530

    class to implement ParkingPlanning

---
+++ src/main/io/java/gojek/parkinglot1/model/planning/NearestParkingPlanning.java
@@ -0,0 +1,26 @@
+package main.io.java.gojek.parkinglot1.model.planning;
+
+
+public class NearestParkingPlanning implements ParkingPlanning{
+
+
+
+    @Override
+    // add free slots to set
+    public void addSlot(int i) {
+
+
+    }
+
+    @Override
+    // get least free slot
+    public int getSlots() {
+        return 0;
+    }
+
+    @Override
+    // remove slot from set once occupied
+    public void removeSlot(int slot) {
+
+    }
+}
