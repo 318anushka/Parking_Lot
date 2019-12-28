63d554fe185937fc0fa9a4bc22be7647c8025d83
Anushka Singh <anushka318singh@gmail.com>
2019-12-28 17:19:15 +0530

    initial commit

--- ParkingLot/src/main/java/io/gojek/parkinglot/model/Vehicle.java
+++
@@ -1,5 +0,0 @@
-package main.java.io.gojek.parkinglot.model;
-
-public class Vehicle {
-
-}
---
+++ src/main/io/java/gojek/parkinglot1/model/Vehicle.java
@@ -0,0 +1,59 @@
+package main.io.java.gojek.parkinglot1.model;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+public abstract class Vehicle implements Externalizable{
+
+    private String registrationNo = null;
+    private String color = null;
+
+    public Vehicle(String registrationNo, String color)
+    {
+        this.registrationNo = registrationNo;
+        this.color = color;
+    }
+
+    // getter for registration no
+    public String getRegistrationNo()
+    {
+        return registrationNo;
+    }
+
+    // getter for color of vehicle
+    public String getColor()
+    {
+        return color;
+    }
+
+    // setter for registration no
+    public void setRegistrationNo(String registrationNo)
+    {
+        this.registrationNo = registrationNo;
+    }
+
+    //setter for color of vehicle
+    public void setColor(String color)
+    {
+        this.color = color;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput o) throws IOException
+    {
+        o.writeObject(getRegistrationNo());
+        o.writeObject(getColor());
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException
+    {
+        setRegistrationNo((String) in.readObject());
+        setColor((String) in.readObject());
+    }
+
+
+
+}
