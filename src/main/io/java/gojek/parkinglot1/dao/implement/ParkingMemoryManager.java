3d55e519d46b087a2cc8894579c395efa7a58c2a
Anushka Singh <anushka318singh@gmail.com>
2019-12-28 19:37:17 +0530

    ParkingMemoryManager:implemented slotMap of given capacity

--- src/main/io/java/gojek/parkinglot1/dao/implement/ParkingMemoryManager.java
+++ src/main/io/java/gojek/parkinglot1/dao/implement/ParkingMemoryManager.java
@@ -1,10 +1,36 @@
 package main.io.java.gojek.parkinglot1.dao.implement;
 
 import main.io.java.gojek.parkinglot1.dao.ParkingDataManager;
+import main.io.java.gojek.parkinglot1.model.Vehicle;
+import main.io.java.gojek.parkinglot1.model.planning.ParkingPlanning;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ParkingMemoryManager implements ParkingDataManager {
+
+    // initial capacity of lot
+    private int capacity = 0;
+    // total available slots in lot
+    private int availability = 0;
+    // instance of ParkingPlanning
+    private ParkingPlanning parkingPlanning;
+
+    private Map<Integer, Vehicle> slotMap;
+
+    public ParkingMemoryManager(int capacity , ParkingPlanning parkingPlanning){
+        this.capacity = capacity;
+        this.availability = capacity;
+        this.parkingPlanning = parkingPlanning;
+        slotMap = new ConcurrentHashMap<>();
+
+        for(int i=1;i<=capacity;i++){
+            slotMap.put(i,null);
+            parkingPlanning.addSlot(i);
+        }
+    }
+
     @Override
     public int parkCar() {
         return 0;
