8568d981178f6945f91713f8d532dc4701f29876
Anushka Singh <anushka318singh@gmail.com>
2019-12-28 18:03:42 +0530

    initialised methods to add,get and remove freeSlots

--- src/main/io/java/gojek/parkinglot1/model/planning/ParkingPlanning.java
+++ src/main/io/java/gojek/parkinglot1/model/planning/ParkingPlanning.java
@@ -2,5 +2,11 @@ package main.io.java.gojek.parkinglot1.model.planning;
 
 public interface ParkingPlanning {
 
+    // creating interface for planning
+    public void addSlot(int i);
+
+    public int getSlots();
+
+    public void removeSlot(int slot);
 
 }
