41dc24e52fc0b7bf075a8519db0078ebb94c2cb8
Anushka Singh <anushka318singh@gmail.com>
2019-12-29 15:46:54 +0530

    main:validate input and provide input format

--- src/main/io/java/gojek/parkinglot1/main.java
+++ src/main/io/java/gojek/parkinglot1/main.java
@@ -2,6 +2,7 @@ package main.io.java.gojek.parkinglot1;
 
 import main.io.java.gojek.parkinglot1.model.Vehicle;
 import main.io.java.gojek.parkinglot1.processor.Execution;
+import main.io.java.gojek.parkinglot1.processor.Validation;
 import main.io.java.gojek.parkinglot1.service.ParkingService;
 import main.io.java.gojek.parkinglot1.service.implement.ServiceImplt;
 
@@ -15,6 +16,7 @@ public class main {
     public static void main(String[] args) throws IOException {
 
         Execution execute = new Execution();
+        Validation validate = new Validation();
         execute.setParkingService(new ServiceImplt());
 
         BufferedReader br = null;
@@ -31,11 +33,44 @@ public class main {
 
             else{
 
-                execute.execute(input);
+                if(validate.validate(input)){
+                    execute.execute(input);}
+
+                else{
+                    inputFormat();
+                }
             }
 
         }
 
 
         }
+
+    private static void inputFormat() {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb = sb.append(
+                "--------------Please Enter one of the below commands. -----------------------")
+                .append("\n");
+        sb = sb.append(" For creating parking lot of size n               ---> create_parking_lot {capacity}")
+                .append("\n");
+        sb = sb
+                .append(" To park a car                                    ---> park <<car_number>> {car_clour}")
+                .append("\n");
+        sb = sb.append(" Unpark car from parking                  ---> leave {slot_number}")
+                .append("\n");
+        sb = sb.append(" Print status of parking slot                     ---> status").append("\n");
+        sb = sb.append(
+                " Get cars registration no for the given car color ---> registration_numbers_for_cars_with_color {car_color}")
+                .append("\n");
+        sb = sb.append(
+                " Get slot numbers for the given car color         ---> slot_numbers_for_cars_with_color {car_color}")
+                .append("\n");
+        sb = sb.append(
+                " Get slot number for the given car number         ---> slot_number_for_registration_number {car_number}")
+                .append("\n");
+        System.out.println(sb.toString());
+
+    }
 }
