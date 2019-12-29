93f4b51159e17b9d67578007c9d41c454160b5d5
Anushka Singh <anushka318singh@gmail.com>
2019-12-29 20:55:16 +0530

    Validation: adding try catch blocks

--- src/main/io/java/gojek/parkinglot1/processor/Validation.java
+++ src/main/io/java/gojek/parkinglot1/processor/Validation.java
@@ -4,45 +4,50 @@ import main.io.java.gojek.parkinglot1.constants.Constants;
 
 public class Validation {
 
-    public boolean validate(String input){
+    public boolean validate(String input)throws Exception{
 
         boolean valid = false;
 
-        String[] inputs = input.split(" ");
-        switch(inputs[0]){
+        try {
+            String[] inputs = input.split(" ");
+            switch (inputs[0]) {
 
-            case Constants.CREATE_PARKING_LOT:
-                valid  = true;
-                break;
+                case Constants.CREATE_PARKING_LOT:
+                    valid = true;
+                    break;
 
-            case Constants.PARK:
-                valid = true;
-                break;
+                case Constants.PARK:
+                    valid = true;
+                    break;
 
-            case Constants.LEAVE:
-                valid = true;
-                break;
+                case Constants.LEAVE:
+                    valid = true;
+                    break;
 
-            case Constants.STATUS:
-                valid = true;
-                break;
+                case Constants.STATUS:
+                    valid = true;
+                    break;
 
-            case Constants.REG_NO_FOR_CARS_WITH_COLOR:
-                valid = true;
-                break;
+                case Constants.REG_NO_FOR_CARS_WITH_COLOR:
+                    valid = true;
+                    break;
 
-            case Constants.SLOTS_NUMBER_FOR_CARS_WITH_COLOR:
-                valid = true;
-                break;
+                case Constants.SLOTS_NUMBER_FOR_CARS_WITH_COLOR:
+                    valid = true;
+                    break;
 
-            case Constants.SLOTS_NUMBER_FOR_REG_NUMBER:
-                valid = true;
-                break;
+                case Constants.SLOTS_NUMBER_FOR_REG_NUMBER:
+                    valid = true;
+                    break;
 
-            default:
-                valid = false;
-                break;
+                default:
+                    valid = false;
+                    break;
 
+            }
+        }
+        catch(Exception e){
+            valid = false;
         }
 
       return valid;
