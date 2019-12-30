4364855423ced387d1f50e3b81a02ef1ce61fd5b
Anushka Singh <anushka318singh@gmail.com>
2019-12-30 20:00:05 +0530

    Validation: check if input string is valid

--- src/main/io/java/gojek/parkinglot1/processor/Validation.java
+++ src/main/io/java/gojek/parkinglot1/processor/Validation.java
@@ -2,6 +2,8 @@ package main.io.java.gojek.parkinglot1.processor;
 
 import main.io.java.gojek.parkinglot1.constants.Constants;
 import main.io.java.gojek.parkinglot1.exception.ParkingException;
+import main.io.java.gojek.parkinglot1.exception.Error;
+
 
 public class Validation {
 
@@ -10,37 +12,72 @@ public class Validation {
         boolean valid = false;
 
         try {
-            String[] inputs = input.split(" ");
-            switch (inputs[0]) {
+            String[] inputs = input.trim().split(" ");
+            int length = inputs.length;
+            switch (length) {
 
-                case Constants.CREATE_PARKING_LOT:
+                case 1: //for status input
 
-                    valid = true;
+                    if(inputs[0].equalsIgnoreCase(Constants.STATUS)){
+                    valid = true;}
                     break;
 
-                case Constants.PARK:
-                    valid = true;
-                    break;
+                case 2: // create parking lot
+                        // leave
+                        // registration no for given color
+                        // slot no for given color
+                        // slot no for given registration no
 
-                case Constants.LEAVE:
-                    valid = true;
-                    break;
 
-                case Constants.STATUS:
-                    valid = true;
-                    break;
+                    if(inputs[0].equalsIgnoreCase(Constants.CREATE_PARKING_LOT)){
+                        try {
+                            int capacity = Integer.parseInt(inputs[1]);
+                            valid = true;
+                            break;
+                        }
+                        catch(Exception e){
+                            valid = false;
+                            break;
+                            //throw new ParkingException(Error.INVALID_VALUE.getMessage() , e);
+                        }
+                    }
 
-                case Constants.REG_NO_FOR_CARS_WITH_COLOR:
-                    valid = true;
-                    break;
+                    else if(inputs[0].equalsIgnoreCase(Constants.LEAVE)){
+                        try {
+                            int slot = Integer.parseInt(inputs[1]);
+                            valid = true;
+                            break;
+                        }
+                        catch(Exception e){
+                            valid = false;
+                            break;
+                            //throw new ParkingException(Error.INVALID_VALUE.getMessage() , e);
+                        }
+                    }
 
-                case Constants.SLOTS_NUMBER_FOR_CARS_WITH_COLOR:
-                    valid = true;
-                    break;
+                    else if(inputs[0].equalsIgnoreCase(Constants.REG_NO_FOR_CARS_WITH_COLOR)){
+                       valid = true;
+                       break;
+                    }
 
-                case Constants.SLOTS_NUMBER_FOR_REG_NUMBER:
-                    valid = true;
-                    break;
+                    else if(inputs[0].equalsIgnoreCase(Constants.SLOTS_NUMBER_FOR_CARS_WITH_COLOR)){
+                        valid = true;
+                        break;
+                    }
+
+                    else if(inputs[0].equalsIgnoreCase(Constants.SLOTS_NUMBER_FOR_REG_NUMBER)){
+                        valid = true;
+                        break;
+                    }
+
+
+                case 3:
+                    if(inputs[0].equalsIgnoreCase(Constants.PARK)){
+
+                        valid = true;
+                        break;
+                    }
+
 
                 default:
                     valid = false;
