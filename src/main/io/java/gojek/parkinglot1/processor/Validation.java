98a24945d93aad6fd9786f6fa6535ac3900aada8
Anushka Singh <anushka318singh@gmail.com>
2019-12-31 11:33:43 +0530

    Validation: modified class to check inputs

--- src/main/io/java/gojek/parkinglot1/processor/Validation.java
+++ src/main/io/java/gojek/parkinglot1/processor/Validation.java
@@ -4,13 +4,18 @@ import main.io.java.gojek.parkinglot1.constants.Constants;
 import main.io.java.gojek.parkinglot1.exception.ParkingException;
 import main.io.java.gojek.parkinglot1.exception.Error;
 import main.io.java.gojek.parkinglot1.constants.InputValidation;
+import main.io.java.gojek.parkinglot1.service.AbstractService;
 
 
-public class Validation {
+public interface Validation {
 
-    public boolean validate(String input)throws ParkingException{
+    public void setService(AbstractService service);
 
-        boolean valid = true;
+    public void execute(String action) throws ParkingException;
+
+    public default boolean validate(String input)throws ParkingException{
+
+        boolean valid = false;
         try
         {
             String[] inputs = input.split(" ");
@@ -18,16 +23,16 @@ public class Validation {
             switch (inputs.length)
             {
                 case 1:
-                    if (key != 0) // e.g status -> inputs = 1
-                        valid = false;
+                    if (key == 0) // e.g status -> inputs = 1
+                        valid = true;
                     break;
                 case 2:
-                    if (key != 1) // create_parking_lot 6 -> inputs = 2
-                        valid = false;
+                    if (key == 1) // create_parking_lot 6 -> inputs = 2
+                        valid = true;
                     break;
                 case 3:
-                    if (key != 2) // park KA-01-P-333 White -> inputs = 3
-                        valid = false;
+                    if (key == 2) // park KA-01-P-333 White -> inputs = 3
+                        valid = true;
                     break;
                 default:
                     valid = false;
